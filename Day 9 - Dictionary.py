fdff64f155fb4f59bbe7757a3df9e31a90ab3c6e
bwadhy <126983316+bwadhy@users.noreply.github.com>
2023-09-27 12:14:34 -0300

    Create Day 9 - Dictionary.py

---
+++ Day 9 - Dictionary.py
@@ -0,0 +1,11 @@
+#Vejamos como podemos utilizar listas para criar tabelas! Desta forma , utilizando essa lógica criarei um simples dicionário.
+
+programming_dictionary = { 
+  "Bug" : "An error in a program that prevents it from running as expected. ",
+  "Function" : "A piece of code that you can easily call over and over again.",
+  "Loop" : "The action of doing something over and over again.",
+}
+#Observar a padronização de boas práticas de leitura. Identação , quebra de linha e vírgula separando.
+
+#Para consultar 👇🏻
+progr

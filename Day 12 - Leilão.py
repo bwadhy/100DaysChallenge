a37d435a4024982f2bee5f0827eb5ff63466e063
bwadhy <126983316+bwadhy@users.noreply.github.com>
2023-10-02 18:35:44 -0300

    Create Day 12 - Leilão.py

---
+++ Day 12 - Leilão.py
@@ -0,0 +1,4 @@
+# Projeto para fazer um leilão cego. Pedir um input de nome e de string, perguntar se mais alguém vai dar um lance. Se sim, limpar, se não repetir o processo.
+
+#Importar um arquivo CSII pra printar um martelo no inicio do game e importar o método clear
+

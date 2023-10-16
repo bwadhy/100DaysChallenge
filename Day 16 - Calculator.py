c3e7179a12519fd38fae1eb4c70036077c9006cd
bwadhy <126983316+bwadhy@users.noreply.github.com>
2023-10-16 19:16:00 -0300

    Update Day 16 - Calculator.py

--- Day 16 - Calculator.py
+++ Day 16 - Calculator.py
@@ -16,3 +16,22 @@ logo = """
 | |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
 |_____________________|
 """
+def add(n1,n2):
+    return n1 + n2 
+
+def subtract(n1,n2):
+    return n1 - n2 
+
+def multiply(n1,n2):
+    return n1 * n2 
+
+def divide(n1,n2):
+    return n1 / n2 
+
+operations = {
+
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide,
+}
